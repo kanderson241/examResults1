b8725c6fdbbb5c0ce870810b7f042cf31b490bde
Guest User <Guest@stk-cs01-ws02.westlothian.sch.uk>
2015-11-19 09:25:05 +0000

    sprint 1 completed and tested

--- PUPIL.java
+++ PUPIL.java
@@ -7,33 +7,46 @@ public class PUPIL
     private String firstName;
     private String surname;
     private int mark;
+    private int pupilDetails;
     
-        public PUPIL()
+    public PUPIL()
     {
         //intialise instance variables
         firstName = "";
         surname = "";
         mark = 0;
-    
-    }
-    
-     public void readPupilDetails(String dataItems)
+  }
+
+    public void readPupilDetails(String dataItems)
    {
     String[] rowItems = dataItems.split(",");
     firstName = rowItems[0];
     surname = rowItems[1];
     mark = Integer.parseInt(rowItems[2]);
-   }
-   
-     public String writeDetails()
+      }
+
+
+    public String writeDetails()
     {
     String pupilData = "";
     pupilData = pupilData.concat(firstName);
     pupilData = pupilData.concat(",");
     pupilData = pupilData.concat(surname);
     pupilData = pupilData.concat(",");
-    pupilData = pupilData.concat(mark);
+    pupilData = pupilData.concat(Integer.toString(mark));
     pupilData = pupilData.concat(",");
     return pupilData;
-    }
-}
+  }
+
+    public float pupilDetails()
+  {
+    return pupilDetails;
+  }
+
+    public void displayDetails()
+  {
+    System.out.println("pupil: "+ firstName + " " + surname);
+    System.out.println(", mark is ");
+    System.out.println();
+  }
+ }
